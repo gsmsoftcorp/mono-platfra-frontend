b29ae97394c361384dc7c9cd4179d9c43397a3f1
kimsd <kimsd1206@naver.com>
2024-03-13 00:54:53 +0900

    테스트

--- frontend/mixins/global.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-
-Vue.mixin({
-  methods: {
-    movePage(name, seq) {
-      this.$router.push({ name: name, params: { seq : seq }})
-    }
-  }
-})
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,67 +1,47 @@
 <template>
-  <div class="container">
     <div>
-      <Logo />
-      <h1 class="title">
-        메인2222
-      </h1>
-      <div class="links">
-        <nuxt-link :to="{ name: 'account-login', params: { }}" class="button--green">
-          로그인
-        </nuxt-link>
-        <nuxt-link :to="{ name: 'master-list', params: { }}" class="button--grey">
-          게시판
-        </nuxt-link>
-      </div>
+        <MainSection title="Home" :loading="loading">
+
+            <Head>
+                <Title>Home / Twitter</Title>
+            </Head>
+
+            <div class="border-b" :class="twitterBorderColor">
+                <TweetForm :user="user" @on-success="handleFormSuccess" />
+            </div>
+
+            <TweetListFeed :tweets="homeTweets" />
+
+        </MainSection>
     </div>
-  </div>
 </template>
+<script setup>
+const { twitterBorderColor } = useTailwindConfig()
+const { getTweets } = useTweets()
 
-<script>
-export default {
-  // lintOnSave: false,
-  name: 'Index'
+const loading = ref(false)
+const homeTweets = ref([])
+const { useAuthUser } = useAuth()
 
+const user = useAuthUser()
+
+onBeforeMount(async () => {
+    loading.value = true
+    try {
+        const { tweets } = await getTweets()
+
+        homeTweets.value = tweets
+    } catch (error) {
+        console.log(error)
+    } finally {
+        loading.value = false
+    }
+})
+
+function handleFormSuccess(tweet) {
+    navigateTo({
+        path: `/status/${tweet.id}`
+    })
 }
+
 </script>
-
-<style>
-  .container {
-    margin: 0 auto;
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-  }
-
-  .title {
-    font-family:
-      'Quicksand',
-      'Source Sans Pro',
-      -apple-system,
-      BlinkMacSystemFont,
-      'Segoe UI',
-      Roboto,
-      'Helvetica Neue',
-      Arial,
-      sans-serif;
-    display: block;
-    font-weight: 300;
-    font-size: 100px;
-    color: #35495e;
-    letter-spacing: 1px;
-  }
-
-  .subtitle {
-    font-weight: 300;
-    font-size: 42px;
-    color: #526488;
-    word-spacing: 5px;
-    padding-bottom: 15px;
-  }
-
-  .links {
-    padding-top: 15px;
-  }
-</style>
