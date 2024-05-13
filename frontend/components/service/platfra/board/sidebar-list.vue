9fed89ad5b65a0777483649c2fb3cf1382e65d54
kimsd <kimsd1206@naver.com>
2024-05-13 20:46:11 +0900

    레이아웃 변경

---
+++ frontend/components/service/platfra/board/sidebar-list.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="group flex items-center rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+         v-for="platfraBoard in platfraBoardList" :key="platfraBoard.platfraBoardSeq"
+         @click="navToName('platfraId-board-platfraBoardSeq', { _platfraBoardSeq: platfraBoard.platfraBoardSeq })" >
+        <span class="truncate cursor-pointer">{{ platfraBoard.subject }}</span>
+    </div>
+</template>
+
+<script setup>
+
+import {ref} from "vue";
+
+const params = {
+    platfraId: useRoute().params.platfraId
+}
+
+const platfraBoardList = ref([]);
+if(useRoute().params.platfraId) {
+    useApiGet('/platfra/board', params).then(response => {
+        platfraBoardList.value = response.data
+    })
+}
+
+</script>
--- frontend/components/service/platfra/content/list.vue
+++ frontend/components/service/platfra/content/list.vue
@@ -1,20 +1,39 @@
 <template>
-    <ul role="list" class="p-10 grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        <li v-for="content in contentList" class="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-left shadow">
-            <div class="flex flex-1 flex-col" @click="navToName('platfraId-content-contentSeq', { _contentSeq: content.contentSeq })">
-                <img class="mx-auto flex-shrink-0" src="https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" alt="" />
-                <h3 class="ml-3 mt-3 text-sm font-medium text-gray-900">{{ content.title }}</h3>
-                <dl class="m-3 flex flex-grow flex-col justify-between">
-                    <dt class="sr-only">Title</dt>
-                    <dd class="text-sm text-gray-500">{{ content.content }}</dd>
-                    <dt class="sr-only">Role</dt>
-<!--                    <dd class="mt-3">-->
-<!--                        <span class="inline-flex items-center rounded-full bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">{{ content.role }}</span>-->
-<!--                    </dd>-->
-                </dl>
+    <div class="bg-white">
+        <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-4 lg:max-w-7xl lg:px-8">
+            <div class="flex items-center justify-between space-x-4">
+                <h2 class="text-lg font-medium text-gray-900">Customers also viewed</h2>
+<!--                <a href="#" class="whitespace-nowrap text-sm font-medium text-indigo-600 hover:text-indigo-500">-->
+<!--                    View all-->
+<!--                    <span aria-hidden="true"> &rarr;</span>-->
+<!--                </a>-->
             </div>
-        </li>
-    </ul>
+            <div class="mt-6 grid grid-cols-1 gap-x-8 gap-y-8 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-4">
+                <div class="group relative"
+                     v-for="content in contentList"
+                     :key="content.contentSeq"
+                     @click="navToName('platfraId-content-contentSeq', { _contentSeq: content.contentSeq })"
+                >
+                    <div class="aspect-h-3 aspect-w-4 overflow-hidden rounded-lg bg-gray-100">
+                        <img class="object-cover object-center" :src="'https://tailwindui.com/img/ecommerce-images/content-page-05-related-content-01.jpg'" :alt="'test'" />
+                        <div class="flex items-end p-4 opacity-0 group-hover:opacity-100" aria-hidden="true">
+                            <div class="w-full rounded-md bg-white bg-opacity-75 px-4 py-2 text-center text-sm font-medium text-gray-900 backdrop-blur backdrop-filter">View content</div>
+                        </div>
+                    </div>
+                    <div class="mt-4 flex items-center justify-between space-x-8 text-base font-medium text-gray-900">
+                        <h3>
+                            <a href="#">
+                                <span aria-hidden="true" class="absolute inset-0" />
+                                {{ content.title }}
+                            </a>
+                        </h3>
+                        <!--                        <p>{{ content.price }}</p>-->
+                    </div>
+                    <p class="mt-1 text-sm text-gray-500">{{ platfraId }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 <script setup lang="ts">
     const platfraId = useRoute().params.platfraId;
@@ -27,81 +46,6 @@
         contentList.value = response.data;
     });
 
-
-    // const contentList = [
-    //     {
-    //         name: 'Jane Cooper',
-    //         title: 'Paradigm Representative',
-    //         role: 'Admin',
-    //         email: 'janecooper@example.com',
-    //         telephone: '+1-202-555-0170',
-    //         imageUrl:
-    //             'https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
-    //     },
-    //     {
-    //         name: 'Jane Cooper',
-    //         title: 'Paradigm Representative',
-    //         role: 'Admin',
-    //         email: 'janecooper@example.com',
-    //         telephone: '+1-202-555-0170',
-    //         imageUrl:
-    //             'https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
-    //     },
-    //     {
-    //         name: 'Jane Cooper',
-    //         title: 'Paradigm Representative',
-    //         role: 'Admin',
-    //         email: 'janecooper@example.com',
-    //         telephone: '+1-202-555-0170',
-    //         imageUrl:
-    //             'https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
-    //     },
-    //     {
-    //         name: 'Jane Cooper',
-    //         title: 'Paradigm Representative',
-    //         role: 'Admin',
-    //         email: 'janecooper@example.com',
-    //         telephone: '+1-202-555-0170',
-    //         imageUrl:
-    //             'https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
-    //     },
-    //     {
-    //         name: 'Jane Cooper',
-    //         title: 'Paradigm Representative',
-    //         role: 'Admin',
-    //         email: 'janecooper@example.com',
-    //         telephone: '+1-202-555-0170',
-    //         imageUrl:
-    //             'https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
-    //     },
-    //     {
-    //         name: 'Jane Cooper',
-    //         title: 'Paradigm Representative',
-    //         role: 'Admin',
-    //         email: 'janecooper@example.com',
-    //         telephone: '+1-202-555-0170',
-    //         imageUrl:
-    //             'https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
-    //     },
-    //     {
-    //         name: 'Jane Cooper',
-    //         title: 'Paradigm Representative',
-    //         role: 'Admin',
-    //         email: 'janecooper@example.com',
-    //         telephone: '+1-202-555-0170',
-    //         imageUrl:
-    //             'https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
-    //     },
-    //     {
-    //         name: 'Jane Cooper',
-    //         title: 'Paradigm Representative',
-    //         role: 'Admin',
-    //         email: 'janecooper@example.com',
-    //         telephone: '+1-202-555-0170',
-    //         imageUrl:
-    //             'https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
-    //     },
-    // ]
 </script>
 <style scoped>
 
